b4d64e8f4d0585b98e559934c056d9fbabcf57fe
Codemonk <vraut12cr@gmail.com>
2024-06-20 13:18:53 +0530

    Microblog Backend done with static data

---
+++ 03_Project1_Microblog/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+import datetime
+
+app = Flask(__name__)
+
+entries = []
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        entry_content = request.form.get("content")
+        formated_date = datetime.datetime.today().strftime("%Y-%m-%d")
+        entries.append((entry_content, formated_date))
+        
+    entries_with_date = [
+        (entry[0],
+        entry[1],
+        datetime.datetime.strptime(entry[1], "%Y-%m-%d").strftime("%b %d")
+        )
+        for entry in entries
+    ]
+    
+    return render_template("home.html", entries=entries_with_date)
